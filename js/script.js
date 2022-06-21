8f39e735de782818cc2b8b1b20efc69a5b5c30f1
KelseyRocco <kelseyroccoh@gmail.com>
2022-06-21 15:42:25 -0400

    Add global variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//GLOBAL VARIABLES
+
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessBtn = document.querySelector(".guess");
+const guessInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesDisplay = document.querySelector(".remaining");
+const numOfRemainingGuesses = document.querySelector(".remaining p");
+const messgage = document.querySelector(".message");
+const playAgainBtn = document.querySelector(".play-again hide");
