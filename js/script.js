9084451e6fd632298fdeebfd8dbdecbb34959242
KelseyRocco <kelseyroccoh@gmail.com>
2022-06-21 16:53:32 -0400

    Accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining p");
-const messgage = document.querySelector(".message");
+const message = document.querySelector(".message");
 const playAgainBtn = document.querySelector(".play-again hide");
 
 const word = "Magnolia";
+const guessedLetters = [];
 
 //FUNCTIONS
+
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
@@ -21,12 +23,37 @@ const placeholder = function (word) {
   wordInProgress.innerText = placeholderLetters.join("");
 };
 
-placeholder(word);
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Guess a letter";
+  } else if (input.length > 1) {
+    message.innerText = "Only one letter please";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Only letters from the alphabet accepted";
+  } else {
+    return input;
+  }
+};
+
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+};
 
 //EVENT HANDLERS
+
 guessBtn.addEventListener("click", function (e) {
   e.preventDefault();
   const guess = letterInput.value;
   console.log(guess);
   letterInput.value = "";
+  message.innerText = "";
+  const playerGuess = validateInput(guess);
+  makeGuess(guess);
 });
