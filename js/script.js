a8624607a9ff4c93fa9d6e0ed8a393e8389c4e27
KelseyRocco <kelseyroccoh@gmail.com>
2022-06-22 16:25:22 -0400

    Display word and guessed letters, win function and reset button

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 //GLOBAL VARIABLES
-
 const guessedLettersElement = document.querySelector(".guessed-letters");
-const guessBtn = document.querySelector(".guess");
+const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
-const remainingGuessesSpan = document.querySelector(".remaining p");
+const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const playAgainBtn = document.querySelector(".play-again hide");
+const playAgainButton = document.querySelector(".play-again");
 
-const word = "Magnolia";
+const word = "magnolia";
 const guessedLetters = [];
 
 //FUNCTIONS
-
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
@@ -23,15 +21,21 @@ const placeholder = function (word) {
   wordInProgress.innerText = placeholderLetters.join("");
 };
 
+placeholder(word);
+
 const validateInput = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
   if (input.length === 0) {
-    message.innerText = "Guess a letter";
+    // Is the input empty?
+    message.innerText = "Please enter a letter.";
   } else if (input.length > 1) {
-    message.innerText = "Only one letter please";
+    // Did you type more than one letter?
+    message.innerText = "Please enter a single letter.";
   } else if (!input.match(acceptedLetter)) {
-    message.innerText = "Only letters from the alphabet accepted";
+    // Did you type a number, a special character or some other non letter thing?
+    message.innerText = "Please enter a letter from A to Z.";
   } else {
+    // We finally got a single letter, omg yay
     return input;
   }
 };
@@ -39,21 +43,60 @@ const validateInput = function (input) {
 const makeGuess = function (guess) {
   guess = guess.toUpperCase();
   if (guessedLetters.includes(guess)) {
-    message.innerText = "You already guessed that letter. Try again.";
+    message.innerText = "You already guessed that letter, silly. Try again.";
   } else {
     guessedLetters.push(guess);
     console.log(guessedLetters);
+    showGuessedLetters();
+    updateWordInProgress(guessedLetters);
   }
 };
 
-//EVENT HANDLERS
+const showGuessedLetters = function () {
+  // Clear the list first
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+  }
+};
 
-guessBtn.addEventListener("click", function (e) {
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    } else {
+      revealWord.push("●");
+    }
+  }
+  wordInProgress.innerText = revealWord.join("");
+  checkIfWin();
+};
+
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+  }
+};
+
+//EVENT LISTENERS
+guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
-  const guess = letterInput.value;
-  console.log(guess);
-  letterInput.value = "";
+  // Empty message paragraph
   message.innerText = "";
-  const playerGuess = validateInput(guess);
-  makeGuess(guess);
+  // Let's grab what was entered in the input
+  const guess = letterInput.value;
+  // Let's make sure that it is a single letter
+  const goodGuess = validateInput(guess);
+
+  if (goodGuess) {
+    // We've got a letter! Let's guess!
+    makeGuess(guess);
+  }
+  letterInput.value = "";
 });
